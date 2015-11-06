bbeceacef9e3fa95be24ca8122e2e7dedbd39581
Jakub Misterka <san.y4ku@gmail.com>
2015-11-06 12:45:11 -0600

    jest integration

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Voting from './components/Macros';
+
+const macros = {
+    protein: 0,
+    fats: 0,
+    carbs: 0
+};
+
+/*ReactDOM.render(
+    null
+    //<Macros macros={macros} />,
+    document.getElementById('app')
+);*/
